74cb2c560318fbe6dc56804c733261f04db4d8b5
ozbekleromer <omerozbekler94@gmail.com>
2020-11-15 05:58:04 +0100

    Added Vet List and updated controller. Closes #15.

--- pet-clinic-web/src/main/java/com/ozbek/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/ozbek/sfgpetclinic/controllers/VetController.java
@@ -1,6 +1,8 @@
 package com.ozbek.sfgpetclinic.controllers;
 
+import com.ozbek.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,8 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
