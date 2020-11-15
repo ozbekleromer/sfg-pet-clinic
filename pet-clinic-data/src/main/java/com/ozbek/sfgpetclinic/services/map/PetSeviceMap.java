f12de26323efec57ef0c7995efae720e2db791be
ozbekleromer <omerozbekler94@gmail.com>
2020-11-15 06:13:44 +0100

    Refactored Map Impl to abstract IdD generation. Closes #23.

--- pet-clinic-data/src/main/java/com/ozbek/sfgpetclinic/services/map/PetSeviceMap.java
+++ pet-clinic-data/src/main/java/com/ozbek/sfgpetclinic/services/map/PetSeviceMap.java
@@ -24,7 +24,7 @@ public class PetSeviceMap extends AbstractMapService<Pet, Long> implements PetSe
 
     @Override
     public Pet save(Pet object) {
-        return super.save(object.getId(), object);
+        return super.save(object);
     }
 
     @Override
