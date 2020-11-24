06aedaa465507ea63964a06c59498522f03fab46
ozbekleromer <omerozbekler94@gmail.com>
2020-11-23 22:42:51 -0800

    Added visits, updated PetType, updated owner with pet relationship. Closes #26.

--- pet-clinic-data/src/main/java/com/ozbek/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/ozbek/sfgpetclinic/model/Owner.java
@@ -1,9 +1,19 @@
 package com.ozbek.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author ozbek on 2020-11-08 23:29
  */
 public class Owner extends Person {
 
+    private Set<Pet> pets;
 
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
--- pet-clinic-data/src/main/java/com/ozbek/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/ozbek/sfgpetclinic/model/PetType.java
@@ -4,4 +4,14 @@ package com.ozbek.sfgpetclinic.model;
  * @author ozbek on 2020-11-08 23:31
  */
 public class PetType extends BaseEntity {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
