7e636d84b30717d5eb1e5e816f1bcff43aa31e25
ozbekleromer <omerozbekler94@gmail.com>
2020-11-25 09:41:57 +0100

    Added Speciality, updated Vet with Speciality relationship. Closes #27.

--- pet-clinic-data/src/main/java/com/ozbek/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/ozbek/sfgpetclinic/model/Vet.java
@@ -1,8 +1,19 @@
 package com.ozbek.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author ozbek on 2020-11-08 23:28
  */
 public class Vet extends Person {
 
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
