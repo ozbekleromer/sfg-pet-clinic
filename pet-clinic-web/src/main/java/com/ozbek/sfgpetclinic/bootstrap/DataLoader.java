c839f40d672653ae7bbbbf9ebb2407b7a5fe1054
ozbekleromer <omerozbekler94@gmail.com>
2020-11-25 10:26:54 +0100

    Added Pets and Contact info to owners on startup. Closes #33.

--- pet-clinic-web/src/main/java/com/ozbek/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/ozbek/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.ozbek.sfgpetclinic.bootstrap;
 
 import com.ozbek.sfgpetclinic.model.Owner;
+import com.ozbek.sfgpetclinic.model.Pet;
 import com.ozbek.sfgpetclinic.model.PetType;
 import com.ozbek.sfgpetclinic.model.Vet;
 import com.ozbek.sfgpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.ozbek.sfgpetclinic.services.PetTypeService;
 import com.ozbek.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 /**
  * created by ozbek on 2020-11-13
@@ -39,12 +42,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("1231231234");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+        owner1.getPets().add(mikesPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner2.setAddress("123 Brickerel");
+        owner2.setCity("Miami");
+        owner2.setTelephone("1231231234");
+
+        Pet fionasCat = new Pet();
+        fionasCat.setPetType(savedCatPetType);
+        fionasCat.setOwner(owner2);
+        fionasCat.setBirthDate(LocalDate.now());
+        fionasCat.setName("Just Cat");
+        owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
 
