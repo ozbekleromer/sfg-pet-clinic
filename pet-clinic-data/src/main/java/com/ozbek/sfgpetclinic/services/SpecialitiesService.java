f8800baa49594badb1d8d5bbd7a7dc1ee03af7b8
ozbekleromer <omerozbekler94@gmail.com>
2020-11-25 10:36:15 +0100

    Added Specialities Map Impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/com/ozbek/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,9 @@
+package com.ozbek.sfgpetclinic.services;
+
+import com.ozbek.sfgpetclinic.model.Speciality;
+
+/**
+ * created by ozbek on 2020-11-25
+ */
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
